4dee009bb7cdbb780bfda0b93d0546de7df84e61
Trevor-Eskes <trevor.Eskes@stu.boone.kyschools.us>
2021-03-12 12:00:36 -0500

    new save

--- Crab.java
+++ Crab.java
@@ -6,11 +6,16 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, and Greenfoot)
 public class Crab extends Actor
 {
     public void act()
-    {
-        
+   {
+        if (isAtEdge ()   )
+        {
+            turn (17); 
+            
+        }
+        move( ); 
     }
     
-    public void move( )
+    public void move ()
     {
         move(5);
     }
